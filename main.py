b8f9a091db5331271b73cb2d95412a24b8880e86
LDYMJ1993 <ldymj1993.cs07g@nctu.edu.tw>
2018-08-15 16:08:28 +0800

    done 核心功能實現完畢

--- main.py
+++ main.py
@@ -1,45 +1,80 @@
 import mechanicalsoup
 import requests
 
+# 初始宣告
 Name = input('教授名稱：')
-Student = int(input('參考的最近研究生數量：'))
-print('您要找的是', Name, '參考的研究生為最近的', Student, '個')
-
-years = [Student]
-
+Input_Student = int(input('參考最近碩士畢業生的數量：'))
+Student = Input_Student
 url = 'http://etd.lib.nctu.edu.tw/cgi-bin/gs32/tugsweb.cgi?o=dwebmge'
+
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(url)
 browser.select_form('form[name="main"]')
+# 填入資料並開始搜尋
 browser["qs0"] = Name
 browser["dcf"] = "ad"
-browser.submit_selected()   # 開始查詢
+browser.submit_selected()
 
 # 紀錄網址中的ccd項
-temp = browser.get_url()
-code = temp[56:62:]
+ccd = browser.get_url()
+ccd = ccd[56:62:]
 
-# descent sort by graduate year && show newest 100 datas
+# 根據畢業年遞減排序
 browser.select_form('form[name="main"]')
-browser["jpsize"] = "100"
 browser["sortby"] = "-yr"
 browser["SubmitChangePage"] = "1"
 browser.submit_selected()
-sdfasdfasdf
 
-# 進入第一項
-enter = "cgi-bin/gs32/tugsweb.cgi/ccd=" + code + "/r"
+# 進入第一筆資料，並取得資料網址
+enter = "cgi-bin/gs32/tugsweb.cgi/ccd=" + ccd + "/r"
 browser.follow_link(enter.strip())
 now = browser.get_url()
 
+# Y2：兩年畢業、Y2_3：兩年以上三年以下畢業，以此類推；previous_number用來紀錄前一筆的學號
+Y2, Y2_3, Y3_4, Y4_beyond, previous_number, i = (0, 0, 0, 0, 0, 0)
 
-# 遞迴找出下一項
-for i in range(1,Student+1):
+# 利用迴圈依序進入每一筆資料
+#for i in range(1,Student+1):
+while i < Student:
+    i += 1
     now = now[:73] + str(i)
-    print(now)
     browser.open(now.strip())
-    # Next：想辦法取出學號、畢業年份資訊並計算
     access = browser.get_current_page()
-    print(i)
-    print(access.body.form.div.table.tbody.tr.td.table.find("th",text="學號:").find_next_sibling().get_text())
-    print(access.body.form.div.table.tbody.tr.td.table.find("th",text="畢業學年度:").find_next_sibling().get_text())
+
+    # 過濾博士生資料
+    degree = access.body.form.div.table.tbody.tr.td.table.find("th",text="學位類別:").find_next_sibling().get_text()
+    if (degree == "博士"):
+        Student += 1
+        continue
+
+    # 取得學號，並偵測第 i 筆資料是否已超過教授收過的學生量
+    number = access.body.form.div.table.tbody.tr.td.table.find("th",text="學號:").find_next_sibling().get_text()
+    if(previous_number == number):
+        break
+    else:
+        previous_number = number
+
+    # 取得畢業學年度
+    grad_year = access.body.form.div.table.tbody.tr.td.table.find("th",text="畢業學年度:").find_next_sibling().get_text()
+
+    # 過濾出學號中的入學年資訊
+    number = number[0:2]
+    if(number[0]=="0"):
+        number = "1" + number
+
+    # 畢業年 - 入學年
+    calculate = int(grad_year) - int(number)
+    if calculate == 1:
+        Y2 += 1
+    elif calculate == 2:
+        Y2_3 += 1
+    elif calculate == 3:
+        Y3_4 += 1
+    else:
+        Y4_beyond += 1
+
+print("最近", Input_Student, "筆碩士畢業生紀錄中")
+print(Y2, "位準時兩年畢業")
+print(Y2_3, "位兩到三年畢業")
+print(Y3_4, "位三到四年畢業")
+print(Y4_beyond, "位四年以上畢業")
